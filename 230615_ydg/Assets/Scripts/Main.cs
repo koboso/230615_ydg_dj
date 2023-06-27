fd20be82a5e3643c998dcc7793a400af0f28b4ce
koboso <34165501+koboso@users.noreply.github.com>
2023-06-27 18:33:00 +0900

    과제해결
    과제해결 230627

--- 230615_ydg/Assets/Scripts/Main.cs
+++ 230615_ydg/Assets/Scripts/Main.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class Main : MonoBehaviour{
 
-    int a;
-
-    int[] arrayNumber = new int[10];
-
-    Enemy[] enemyList = new Enemy[10];
-
-
-    List<int> listNumber = new List<int>();
-
-    List<Enemy> eList = new List<Enemy>();
-
-
-
+    List<Character> characterList;
 
 
     void Start(){
-        for(int i =0; i< enemyList.Length; i++){
+        characterList = new List<Character>();
 
-            Enemy e = new Enemy("elf", i);
-            e.GetEnemyInfo();
-
-            eList.Add(e);
+        PrintAllCharacter();
 
 
+        CreateCharacter(new Character("ashe", "archer", 100, 30));
+        CreateCharacter(new Character("teemo", "thief", 80, 20));
+        CreateCharacter(new Character("ezreal", "mage", 70, 100));
+
+        RemoveCharacter(2);
+        CreateCharacter(new Character("mesh", "knight", 300, 10));
+
+        PrintAllCharacter();
+    }
+
+
+    public void CreateCharacter(Character c){
+        characterList.Add(c);
+    }
+
+
+    public void RemoveCharacter(int index){
+        characterList.RemoveAt(index);
+    }
+
+    public void PrintAllCharacter(){
+
+        int i = 0; 
+
+        if(characterList.Count == 0)
+        {
+            Debug.Log("비어있습니다.");
+        }
+        else
+        {
+            while (i < characterList.Count)
+            {
+                characterList[i].GetInfo();
+                i++;
+            }
         }
 
-
-
-
+        
     }
 
 }
