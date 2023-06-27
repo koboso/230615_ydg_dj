5a608c9a875de9a1d7ae08834d2990bbba02aa3e
koboso <34165501+koboso@users.noreply.github.com>
2023-06-27 19:04:24 +0900

    Enemy Class 구조 생성
    Enemy Class 구조 생성

--- 230615_ydg/Assets/Scripts/Enemy.cs
+++ 230615_ydg/Assets/Scripts/Enemy.cs
@@ -1,19 +1,48 @@
 using System;
 
 public class Enemy {
-    private string name;
+    public enum EnemyType{
+        Basic = 0,
+        Range,
+    }
+    private EnemyType enemyType;
     private int hp;
+    private int atk;
+    private float speed;
 
-
-    public Enemy(string name, int hp) {
-        this.name = name;
+    public Enemy(EnemyType type, int hp, int atk, float speed) {
+        this.enemyType = type;
         this.hp = hp;
+        this.atk = atk;
+        this.speed = speed;
     }
 
 
-
-    public void GetEnemyInfo(){
-        UnityEngine.Debug.Log($"name : {this.name}  attack : {this.hp}");
+    public int Hp
+    {
+        get { return this.hp; }
     }
+
+    public int Atk
+    {
+        get { return this.atk; }
+    }
+
+    public float Speed
+    {
+        get { return this.speed; }
+    }
+
+    public bool Hit(int dmg){
+        if (this.hp - dmg <= 0) return false;
+
+        this.hp -= dmg;
+
+        return true;
+    }
+    
+
+
+
 
 }
