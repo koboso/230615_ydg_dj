0c158c6e1ce950384b4ac1593ea5cd40ed2dc59a
koboso <34165501+koboso@users.noreply.github.com>
2023-07-11 19:53:29 +0900

    input code 추가
    ㅁㅁ

--- 230615_ydg/Assets/Scripts/PlayerMove.cs
+++ 230615_ydg/Assets/Scripts/PlayerMove.cs
@@ -11,13 +11,19 @@ public class PlayerMove : MonoBehaviour{
     // Update is called once per frame
     void Update(){
 
+        Debug.Log("update Began");
+
         if (GameCore.Instance.gameStatus != GameCore.GameStatus.Play) return;
 
+        Debug.Log("Update In");
 
-#if UNITY_EDITOR
+#if UNITY_EDITOR_WIN
+        Debug.Log("Update Window");
         WindowPlatform();
 #elif PLATFORM_ANDROID
         AndroidPlatform();
+        Debug.Log("Update Android");
+
 #endif
     }
 
@@ -58,8 +64,48 @@ public class PlayerMove : MonoBehaviour{
     }
 
 
-    private void AndroidPlatform()
-    {
+    private void AndroidPlatform(){
+
+
+       if(Input.touchCount >= 1)
+        {
+
+            Debug.Log("Touch IN");
+
+            switch (Input.GetTouch(0).phase)
+            {
+                case TouchPhase.Began:
+                    Debug.Log("Began");
+                    isDragging = true;
+                    currentPos = this.transform.position - ConvertMousePosition();
+                    break;
+               
+                case TouchPhase.Ended:
+
+                    Debug.Log("ended");
+                    isDragging = false;
+                    break;
+            }
+
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+                
+
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+             
+            }
+
+            */
+
+
+            if (isDragging)
+            {
+                this.transform.position = ConvertMousePosition() + currentPos;
+            }
+        }
 
     }
 
